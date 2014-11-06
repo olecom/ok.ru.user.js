56168bf2471a9fa30c99ac51d1d10484d2b1f6b2
Oleg Verych <olecom@gmail.com>
2014-11-06 10:36:08 +0400

    v0.4 в ленте: нет рекламы, и достижений в играх

--- ok_no_ads.user.js
+++ ok_no_ads.user.js
@@ -6,11 +6,33 @@
 // @match           http://www.odnoklassniki.ru/*
 // @match           http://odnoklassniki.ru/*
 // @match           http://ok.ru/*
-// @version         0.3
+// @version         0.4
 // ==/UserScript==
 
 function id(n){
 return document.getElementById(n)
+}
+
+function rmgames(cls){
+var i, t, arr = document.querySelectorAll("." + cls)
+    if(arr && arr.length) for(i = 0; i < arr.length; ++i){
+        t = arr[i]
+        if(~t.innerHTML.indexOf('в игре')){
+            t.parentElement.innerHTML=''
+        }
+    }
+    t = arr = void 0
+}
+
+function rmads(cls){
+var i, t, arr = document.querySelectorAll("." + cls)
+    if(arr && arr.length) for(i = 0; i < arr.length; ++i){
+        t = arr[i]
+        if(~t.innerText.indexOf('Реклама')){
+            t.parentElement.parentElement.innerHTML=''
+        }
+    }
+    t = arr = void 0
 }
 
 function rmq(cls){
@@ -40,5 +62,7 @@ try {
     rmq("dialogWrapperBanner")
     rmq('__gift')
     rmq('mctc_link')
+    rmads("feed-i_spec-lbl")
+    rmgames('feed-i_action')
 } catch(e) { }
 }, 1234)
